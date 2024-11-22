a2d38d43d90b5e5c789009e3c00e9fc2e3a75f94
MutedHunter <334821@eriesd.org>
2024-11-22 12:50:30 -0500

    god polease ghelp me

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,12 +19,14 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 # Up two folders to serve "site" content
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SITE_ROOT = os.path.join(BASE_DIR, 'site')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('polls/', include('polls.urls')),
     path('hello/', include('hello.urls')),
     path('autos/', include('autos.urls')),
